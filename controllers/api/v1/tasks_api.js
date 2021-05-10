edd2263a147e625ed2e17e1d90fcae4d697344a2
Prashwar Mishra <prashwarmishra@gmail.com>
2021-05-10 15:35:17 +0530

    create API for deleting and updating todos

--- controllers/api/v1/tasks_api.js
+++ controllers/api/v1/tasks_api.js
@@ -49,3 +49,51 @@ module.exports.getTodo = async function(req, res){
         });
     }
 }
+
+module.exports.updateTask = async function(req, res){
+    try {
+        let task = await Task.findById(req.params.id);
+        
+        if(task.user == req.user.id){
+            if(task){
+                task.description = req.body.new_description;
+                task.dueDate = req.body.new_dueDate;
+                task.category = req.body.new_category;
+                task.save();
+            }
+            return res.status(200).json({
+                message: 'Task updated successfully',
+            });
+        }
+    } catch (error) {
+        console.log('Error in deleting task:', error);
+        return res.status(500).json({
+            message: 'Internal Server Error',
+        });
+    }
+}
+
+module.exports.deleteTask = async function(req, res){
+    try {
+        let task = await Task.findById(req.params.id);
+        console.log(req.params.id);
+        if(req.user.id == task.user){
+            task.remove();
+            await User.findByIdAndUpdate(req.user.id, {$pull: {tasks: req.params.id}});
+
+            return res.status(200).json({
+                message: 'Task deleted Successfully',
+            });
+        }else{
+            return res.status(422).json({
+                message: 'Unauthorized',
+            });
+        } 
+    } catch (error) {
+        console.log('Error in deleting task:', error);
+        return res.status(500).json({
+            message: 'Internal Server Error',
+        });
+    }
+    
+}
--- routes/api/v1/tasks.js
+++ routes/api/v1/tasks.js
@@ -5,5 +5,7 @@ const tasksApi = require('../../../controllers/api/v1/tasks_api');
 
 router.get('/', passport.authenticate('jwt', {session: false}), tasksApi.getTodo);
 router.post('/add-task', passport.authenticate('jwt', {session: false}), tasksApi.addTask);
+router.post('/update-task/:id', passport.authenticate('jwt', {session: false}), tasksApi.updateTask);
+router.delete('/delete-task/:id', passport.authenticate('jwt', {session: false}), tasksApi.deleteTask);
 
 module.exports = router;
